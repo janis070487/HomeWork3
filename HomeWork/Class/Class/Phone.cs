ba0e97909476dd1daefc7726cb794c90fb6e6368
janis070487 <janis070487@gmail.com>
2022-04-24 16:14:58 +0300

    First task add

---
+++ HomeWork/Class/Class/Phone.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class
+{
+    internal class Phone
+    {
+
+
+        private double width;
+        private double height;
+        private double lenght;
+        private string brand;
+        private string model;
+
+        public string Model
+        {
+            get { return model; }
+            set { model = value; }
+        }
+        public string Brand { get { return brand; } }
+        public double Width { get { return width; } set { width = value; } }
+        public double Height { get { return height; } set { height = value; } }
+        public double Lenght { get { return lenght; } set { lenght = value; } }
+        public Phone(string _brand)
+        {
+            brand = _brand;
+        }
+
+        public void SendSms(string number, string mesage)
+        {
+            Console.WriteLine($"Jus nosūtījāt ziņu {"\n"}{mesage}{"\n"} uz numuru > {number}");
+        }
+
+        public void Call(string numbers)
+        {
+            Console.WriteLine($"Jūs zvanāt uz numuru > {numbers}");
+        }
+
+        public void GetInfo()
+        {
+            Console.WriteLine($"Telefona Zīmols  : {brand}{"\t"}Telefona Modelis : {model}");
+            Console.WriteLine($"telefona izmērs{"\n"}Platums : {width}{"\n"}Garums : {lenght}{"\n"}Augstums : {height}");
+            double volume = width * height * lenght;
+            Console.WriteLine($"Telefona tilpums : {volume}");
+        }
+
+
+    }
+}
--- HomeWork/Class/Class/Program.cs
+++ HomeWork/Class/Class/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Class;
+Console.WriteLine("Lai izveidotu jautu telefonu ievadiet viņa brendu nosaukumu" + "\n");
+string brandname = Console.ReadLine();
+Phone phone = new Phone(brandname);
+
+Console.WriteLine($"{"\n"}Ievadiet telefona modeļa nosaukumu");
+phone.Model = Console.ReadLine();
+
+Console.WriteLine($"{"\n"}Ievadiet telefona platumu");
+phone.Width = double.Parse(Console.ReadLine());
+
+Console.WriteLine($"{"\n"}Ievadiet telefona garumu");
+phone.Lenght = double.Parse(Console.ReadLine());
+
+Console.WriteLine($"{"\n"}ievadiet lelefona augstumu");
+phone.Height = double.Parse(Console.ReadLine());
+
+Console.WriteLine();
+phone.GetInfo();
+
+Console.WriteLine($"{"\n"}Ievadiet ziņu lai nosūtītu");
+string mesage = Console.ReadLine();
+Console.WriteLine($"{"\n"}Ievadiet adresāta numuru");
+string number = Console.ReadLine();
+Console.WriteLine();
+phone.SendSms(number, mesage);
